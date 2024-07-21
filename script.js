d339eb5b708a639564007b2c0966441938e42997
nico-ind <167833824+nico-ind@users.noreply.github.com>
2024-07-21 19:58:02 -0300

    Add a button which will reset the grid with a new size entered by user

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
-// User input for grid size
-const gridSize = 16;
+function createGrid(gridSize) {
+    // for loop to create grid
+    for (let i = 0; i < gridSize; i++) {
+
+        // create row div element 
+        const rowContainer = document.createElement('div');
+        
+        // Add class to container
+        rowContainer.classList.add('rowDiv');
+
+            for (let i = 0; i < gridSize; i++) {
+                // create element div
+                const rowELement = document.createElement('div'); 
+
+                // Add class to container
+                rowELement.classList.add('elementDiv');
+                rowContainer.appendChild(rowELement);
+
+                // Add hover event listener to every rowELement
+                rowELement.addEventListener('mouseover', () => {
+                    rowELement.style.backgroundColor = 'black'
+                })
+            }
+
+        container.appendChild(rowContainer);
+    }
+}
 
 // Create reference for general div
 const container = document.querySelector('#container');
 
-// for loop to create grid
-for (let i = 0; i < gridSize; i++) {
+// Defalt value for grid size
+const gridSize = 16;
 
-    // create row div element 
-    const rowContainer = document.createElement('div');
-    
-    // Add class to container
-    rowContainer.classList.add('rowDiv');
+// Run at least once
+createGrid(gridSize);
 
-        for (let i = 0; i < gridSize; i++) {
-            // create element div
-            const rowELement = document.createElement('div'); 
+// Connect function to button
+btn = document.querySelector('button');
 
-            // Add class to container
-            rowELement.classList.add('elementDiv');
-            rowContainer.appendChild(rowELement);
+btn.addEventListener('click', () => {
 
-            // Add hover event listener to every rowELement
-            rowELement.addEventListener('mouseover', () => {
-                rowELement.style.backgroundColor = 'black'
-            })
-        }
+    // get user input on new grid size
+    const newGridSize = prompt('Please enter a new Grid Size');
 
-    container.appendChild(rowContainer);
-}
+    // Limit max grid size and solve for negative inputs
+    if (newGridSize > 100) {
+        newGridSize = 100;
+    } else if (newGridSize < 0) {
+        newGridSize = newGridSize * -1;
+    }
+
+    // Delete all previous elements
+    const removedElements = document.querySelectorAll('.rowDiv')
+    removedElements.forEach(element => {
+        element.remove();
+    })
+
+    createGrid(newGridSize)
+})
+
+
