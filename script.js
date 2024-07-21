2ff450117ec147d8186cdb5086843ef6a38479f7
nico-ind <167833824+nico-ind@users.noreply.github.com>
2024-07-21 20:21:12 -0300

    Improve overall style and add random colors and opacity effects

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function createGrid(gridSize) {
+
+    // Create reference for general div
+    const container = document.querySelector('#container');
+
     // for loop to create grid
     for (let i = 0; i < gridSize; i++) {
 
@@ -17,17 +21,20 @@ function createGrid(gridSize) {
                 rowContainer.appendChild(rowELement);
 
                 // Add hover event listener to every rowELement
+                let a = 0;
                 rowELement.addEventListener('mouseover', () => {
-                    rowELement.style.backgroundColor = 'black'
+                    // Randomize color and add opacity in each iter
+                    const r = Math.random() * 255;
+                    const g = Math.random() * 255;
+                    const b = Math.random() * 255;
+                    a += 0.1;
+                    rowELement.style.backgroundColor = `rgb(${r}, ${g}, ${b}, ${a})`;
                 })
             }
 
         container.appendChild(rowContainer);
     }
 }
-
-// Create reference for general div
-const container = document.querySelector('#container');
 
 // Defalt value for grid size
 const gridSize = 16;
@@ -38,6 +45,7 @@ createGrid(gridSize);
 // Connect function to button
 btn = document.querySelector('button');
 
+// Add reset function to button
 btn.addEventListener('click', () => {
 
     // get user input on new grid size
